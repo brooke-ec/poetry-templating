0615cb7e61258def5293fb75905b86be4c6570a4
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2023-12-28 18:14:34 +0000

    feat: switch to evaluating in temporary directory

--- src/poetry_templating/__init__.py
+++ src/poetry_templating/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "${pyproject.tool.poetry.version}"
 
 CONFIG_TABLE = "poetry-templating"
+DEFAULT_BUILD_DIR = "dist"
 DEFAULT_ENCODING = "utf-8"
 DEFAULT_INCLUDE = ["*.py"]
 DEFAULT_EXCLUDE = []
--- src/poetry_templating/plugin.py
+++ src/poetry_templating/plugin.py
@@ -1,23 +1,23 @@
-import logging
+import os
+import shutil
+import tempfile
 from contextlib import contextmanager
-from io import BytesIO, StringIO
-from os import stat_result
 from pathlib import Path
-from tarfile import TarFile, TarInfo
-from typing import IO as IOType
-from typing import Iterator, Union, cast
+from typing import Iterator
 
+from cleo.events.console_command_event import ConsoleCommandEvent
+from cleo.events.console_events import COMMAND
 from cleo.io.io import IO
 from poetry.console.application import Application
+from poetry.console.commands.build import BuildCommand
 from poetry.console.commands.command import Command
-from poetry.core.masonry.builder import Builder
+from poetry.factory import Factory
 from poetry.plugins.application_plugin import ApplicationPlugin
+from poetry.poetry import Poetry
 from poetry.puzzle.provider import Indicator
 
-from poetry_templating.error import TemplatingError
+from poetry_templating import DEFAULT_BUILD_DIR
 from poetry_templating.util import Mixin
-
-_log = logging.getLogger(__name__)
 
 
 @contextmanager
@@ -28,106 +28,71 @@ def progress(io: IO, message: str) -> Iterator[None]:
     else:
         indicator = Indicator(io, "{message}{context}<debug>({elapsed:2s})</debug>")
 
-        with indicator.auto(
-            f"<info>{message}</info>",
-            f"<info>{message}</info>",
-        ):
+        with indicator.auto(message, message):
             yield
 
 
 class EvaluateCommand(Command):
     name: str = "templating evaluate"
-    description = "Evaluate template slots in the current directory."
+    description = "Evaluate templates in the current directory."
 
     def handle(self) -> int:
         from poetry_templating.engine import TemplatingEngine
 
         engine = TemplatingEngine(self.poetry.pyproject)
-        with progress(self.io, "Evaluating template slots..."):
+        with progress(self.io, "<info>Evaluating templates...</info>"):
             count = engine.evaluate_and_replace()
-        self.line(f"<info>Evaluated template slots in {count} files!</info>")
+        self.line(f"<info>Evaluated templates in {count} files!</info>")
         return 0
 
 
 class TemplatingPlugin(ApplicationPlugin):
+    @property
+    def commands(self) -> list[type[Command]]:
+        return [EvaluateCommand]
+
     def activate(self, application: Application):
-        builder_mixin.inject()  # Inject builder mixin so that templating is setup when a build is started.
-        application.command_loader.register_factory(
-            EvaluateCommand.name, lambda: EvaluateCommand()
-        )
+        application.event_dispatcher.add_listener(COMMAND, self.on_command)  # type: ignore
+        self.root = Path(os.path.dirname(application.poetry.pyproject.path))
+        self.poetry = application.poetry
+        super().activate(application)
 
+    def on_command(self, event: ConsoleCommandEvent, *_) -> None:
+        # If command is build command, set up build templating
+        command = event.command
+        if isinstance(command, BuildCommand):
+            self.setup_build(command)
 
-# Mixin to be injected into the Builder.build method to set up the templating system.
-@Mixin.mixin(Builder, "build")
-def builder_mixin(builder: Builder, *args, **kwargs):
-    _log.debug("Setting up Templating System...")
-    from poetry_templating.engine import TemplatingEngine
+    def setup_build(self, command: BuildCommand):
+        from poetry.core.masonry.builder import Builder
 
-    # Set up templating engine
-    engine = TemplatingEngine(builder._poetry.pyproject)
+        # Mixin to point builders to evaluated clone project
+        @Mixin.mixin(command, "handle")
+        def handler_mixin() -> int:
+            with builder_mixin, self.evaluated_clone(command.io) as poetry:
+                command._poetry = poetry
+                return handler_mixin.original()
 
-    # Define replacement for Path.open method
-    @Mixin.mixin(Path, "open")
-    def open_mixin(path: Path, mode: str = "r", *args, **kwargs) -> IOType:
-        relative = engine.relative(path)
-        in_cache = relative in engine.cache
-        writable = mode not in ("r", "rb")
+        # Mixin to set the "target_dir" parameter back to the original project
+        @Mixin.mixin(Builder, "build")
+        def builder_mixin(*args, target_dir: Path | None = None, **kwargs) -> None:
+            target_dir = self.root / DEFAULT_BUILD_DIR
+            builder_mixin.original(*args, **kwargs, target_dir=target_dir)
 
-        try:
-            if in_cache and not writable:
-                evaluated = engine.cache[relative]
-            else:
-                src: IOType = open_mixin.original(path, mode, *args, **kwargs)
-                if writable or not engine.should_process(path):
-                    return src
+        handler_mixin.inject()  # Inject handler mixin to this instance of BuildCommand
 
-                raw: Union[str, bytes] = src.read()
-                if isinstance(raw, bytes):
-                    raw = cast(bytes, raw).decode(engine.encoding)
-                evaluated = engine.evaluate_data(raw, path)  # type: ignore
+    @contextmanager
+    def evaluated_clone(self, io: IO) -> Iterator[Poetry]:
+        from poetry_templating.engine import TemplatingEngine
 
-            # Process file, considering if it was opened in binary mode
-            if mode == "r":
-                return StringIO(evaluated)
-            else:
-                return BytesIO(evaluated.encode(engine.encoding))
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with progress(io, "Evaluating templates..."):
+                shutil.copytree(self.root, tmpdir, symlinks=False, dirs_exist_ok=True)
 
-        except Exception as e:
-            if isinstance(e, TemplatingError):
-                raise
-            raise TemplatingError(
-                f'Error processing template: {e}\n  File "{relative}"'
-            ) from e
+                # Create clone poetry instance
+                poetry = Factory().create_poetry(Path(tmpdir), io=io)
 
-    # Define replacement for TarFile.gettarinfo method
-    @Mixin.mixin(TarFile, "gettarinfo")
-    def tar_info_mixin(tarfile, path, *args, **kwargs) -> TarInfo:
-        info: TarInfo = tar_info_mixin.original(tarfile, path, *args, **kwargs)
-        if engine.should_process(path):
-            info.size = len(Path(path).read_bytes())
-        return info
-
-    # Define replacement for Path.stat method
-    @Mixin.mixin(Path, "stat")
-    def stat_mixin(path: Path, *args, **kwargs):
-        info: stat_result = stat_mixin.original(path, *args, **kwargs)
-        if engine.should_process(path):
-            size = len(Path(path).read_bytes())
-            info = stat_result(
-                [
-                    info.st_mode,
-                    info.st_ino,
-                    info.st_dev,
-                    info.st_nlink,
-                    info.st_uid,
-                    info.st_gid,
-                    size,
-                    info.st_atime,
-                    info.st_mtime,
-                    info.st_ctime,
-                ]
-            )
-        return info
-
-    with open_mixin, tar_info_mixin, stat_mixin:  # Inject mixins for duration of the build
-        return builder_mixin.original(builder, *args, **kwargs)
+                # Evaluate clone
+                engine = TemplatingEngine(poetry.pyproject)
+                engine.evaluate_and_replace()
+            yield poetry
